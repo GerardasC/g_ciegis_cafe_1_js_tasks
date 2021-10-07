03890ff68749636007ba26f1eef144057f56d29a
Gerardas Ciegis <gerardasc@gmail.com>
2021-10-07 20:51:56 +0300

    js-iteration-and-classes-task: done

--- js-iteration-and-classes-task.js
+++ js-iteration-and-classes-task.js
@@ -8,45 +8,94 @@ class Car {
     this.price = price;
   }
 
-  getfuelType = () => this.fuelTypes;
+  getfuelType = () => this.fuelTypes.join('/');
 }
 
-const car1 = new Car('Audi', 'A1', 2015, 'red', 'petrol', 9000);
-const car2 = new Car('Opel', 'Astra', 2015, 'silver', 'diesel', 3000);
-const car3 = new Car('Audi', 'Electric', 1991, 'blue', 'electric', 400);
-const car4 = new Car('BMW', 'x5', 2020, 'brown', 'petrol', 80000);
-const car5 = new Car('Skoda', 'Fabia', 2015, 'yellow', 'diesel', 8000);
-const car6 = new Car('Mazda', '3', 2010, 'black', 'diesel', 4000);
-const car7 = new Car('Tesla', 'ModelX', 2019, 'silver', 'electric', 20000);
-const car8 = new Car('Prosche', '911', 2020, 'white', 'petrol', 90000);
+let cars;
 
-const cars = [car1, car2, car3, car4, car5, car6, car7, car8]
+const usd_eur = 1.16;
 
-cars.forEach(car => console.log(`${car.brand} ${car.model} - ${car.getfuelType()}`))
-console.log('------------------------------')
+console.group('1. Sukurkite klasę Car, kuri turėtų savybes ir priskirtų jas naudojant konstruktorių');
+{
+  console.dir(Car);
+}
+console.groupEnd();
 
-let electricCars = cars.filter(car => car.fuelTypes === 'electric');
-console.log('5 Elektriniai auto: ', electricCars);
+console.group('2. Sukurkite masyvą sudarytą iš 8 Car klasės mašinų');
+{
+  cars = [
+    new Car('Toyota', 'Yaris', 2002, 'red', ['dysel'], 800),
+    new Car('Tesla', 'Model 3', 2016, 'black', ['electric'], 22000),
+    new Car('Toyota', 'Avensis', 2015, 'blue', ['petrol'], 13000),
+    new Car('Audi', 'A6', 2015, 'black', ['petrol'], 14000),
+    new Car('Audi', 'A5', 2015, 'red', ['petrol'], 17000),
+    new Car('Audi', 'R8', 2019, 'black', ['petrol'], 17000),
+    new Car('Audi', 'R8', 2017, 'black', ['petrol'], 15000),
+    new Car('Tesla', 'Model S', 2020, 'blue', ['electric'], 38000),
+    new Car('Subaru', 'Impreza', 2010, 'green', ['dysel', 'electric'], 6000)
+  ];
+  console.table(cars);
+}
+console.groupEnd();
 
-let petrolCars = cars.filter(car => car.year > 2016 && car.fuelTypes === 'petrol');
-console.log('6 Benzininiai auto: ', petrolCars);
+console.group('3. Klasėje Car sukurkite metodą getFuelType, kuris grąžintų mašinos kurų tipus (fuelTypes) kaip \'string\'ą atskirtą \'/\'.');
+{
+  cars.forEach(car => console.log(car.getfuelType()));
+}
+console.groupEnd();
 
-let audi = cars.filter(car => car.brand === 'Audi' && ((2012 <= car.year) && (car.year <= 2016)));
-console.log('7 Audi 2012-2016: ', audi);
+console.group('4. Atspausdinkite kiekvieno automobilio pavadinimą ir 3 punktu sukurto metodo rezultatą');
+{
+  cars.forEach(car => console.log(`${car.brand} ${car.model} - ${car.getfuelType()}`))
+}
+console.groupEnd();
 
-let averagePrice = electricCars.reduce((prev, car) => prev + car.price, 0) / electricCars.length;
-console.log('8 kainos vidurks: ', averagePrice)
+let electricCars;
+console.group('5. Atrinkite tik elektrinius automobilius');
+{
+  electricCars = cars.filter(car => car.fuelTypes.includes('electric') && car.fuelTypes.length === 1);
+  console.log(electricCars)
+}
+console.groupEnd();
 
-let totalValue = petrolCars.reduce((prev, cars) => cars.price + prev, 0);
-console.log('9 bendra vertė: ', totalValue)
+let newPetrolCars;
+console.group('6. Atrinkite tik benzininius automobilius, naujasnius nei 2016 metai');
+{
+  newPetrolCars = cars.filter(car => car.fuelTypes.includes('petrol') && car.year > 2016);
+  console.log(newPetrolCars);
+}
+console.groupEnd();
 
-let newCars = cars.map(({brand, model, price, fuelTypes}) => ({
-  brand,
-  model,
-  price,
-  fuelTypes
-}))
-console.log('10 ', cars, newCars)
+console.group('7. Atrinkite \'audi\' markės automobilius nuo 2012 iki 2016, kurie nėra dyzeliniai ');
+{
+  console.log(cars.filter(car => car.brand === 'Audi' && car.year >= 2012 && car.year <= 2016 ));
+
+}
+console.groupEnd();
+
+console.group('8. Suskaičiuokite 5 punkte gautų automobilių kainos vidurkį');
+{
+  console.log(electricCars.reduce((prev, car) => car.price + prev, 0) / electricCars.length);
+}
+console.groupEnd();
+
+console.group('9. Suskaičiuokite 6 punkte gautų automobilių bendrą vertę');
+{
+  console.log(newPetrolCars.reduce((prev, car) => prev + car.price, 0));
+}
+console.groupEnd();
+
+console.group('10. Panaudokite pradinį Car objektų masyvą performuoti duomenis į tokių objektų masyvą');
+{
+  const result = cars.map(({brand, model, price, getfuelType}) => ({
+    brand,
+    model,
+    price,
+    fuelType: getfuelType()
+  }));
+  console.log(result);
+}
+console.groupEnd();
 
 /*1. Sukurkite klasę Car, kuri turėtų savybes ir priskirtų jas naudojant konstruktorių:
   brand,
